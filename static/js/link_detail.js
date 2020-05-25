a2862c2b188632ffcf9639e0ef5642a679bb2892
xtess <zhekaa2012@yandex.ru>
2020-05-25 17:33:26 +0300

    Добавлено модальное окно изменения полной ссылки

--- static/js/link_detail.js
+++ static/js/link_detail.js
@@ -4,4 +4,8 @@ $(document).ready(function(){
   $('#title-save-modal').on('click', function(){
     alert($('#title-value-modal').val())
   })
+
+  $('#full-link-save-modal').on('click', function(){
+    alert($('#full-link-value-modal').val())
+  })
 })
